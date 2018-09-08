a7e91ea19bcbb0ddfc4c691b3d8078a34473f1d6
FabrizioCafolla <fabrizio.cafolla97@gmail.com>
2018-09-08 09:57:02 +0200

    delete command in prod

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,12 +12,7 @@ class Kernel extends ConsoleKernel
      *
      * @var array
      */
-	protected $commands = [
-		Commands\CreateRepositroy::class,
-		Commands\CreateApiController::class,
-		Commands\CreateTransformer::class,
-		Commands\CreateExample::class
-	];
+	protected $commands = [];
 
     /**
      * Define the application's command schedule.
