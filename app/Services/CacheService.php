604f5d5d832c1090dd88f536a0c6f7699219d683
FabrizioCafolla <fabrizio.cafolla97@gmail.com>
2018-09-25 19:53:43 +0200

    Add method for many put and get

--- app/Services/CacheService.php
+++ app/Services/CacheService.php
@@ -13,7 +13,6 @@
 	use \Illuminate\Http\Response;
 	use Illuminate\Cache\CacheManager;
 	use Illuminate\Support\Facades\Cache;
-	use Symfony\Component\HttpFoundation\BinaryFileResponse;
 
 	class CacheService
 	{
@@ -41,6 +40,9 @@
 		}
 
 		/**
+		 * Cache creation through serialization in json coding.
+		 * If you want to cache response you pass the $data parameter as a Response instance
+		 *
 		 * @param string $key
 		 * @param $data
 		 * @param string $type
@@ -58,10 +60,26 @@
 		}
 
 		/**
+		 * Method for creating multiple items in the cache
+		 * Example ['key' => $value, 'key2' => $value2]
+		 *
+		 * @param array $values
+		 * @param $type
+		 * @param int $minutes
+		 */
+		public function putMany(array $values, $type, $minutes = 0)
+		{
+			foreach ($values as $key => $data)
+				$this->put($key, $data, $type, $minutes);
+		}
+
+		/**
+		 * Private method for serialization, implements a control to correctly cache responses.
+		 *
 		 * @param $response
 		 * @return string
 		 */
-		private function serialize($response) :string
+		private function serialize($response): string
 		{
 			if (!($response instanceof Response))
 				return json_encode($response);
@@ -78,6 +96,21 @@
 		}
 
 		/**
+		 * Method for taking multiple keys together, returns a key associative array => value
+		 *
+		 * @param array $values
+		 * @param $type
+		 */
+		public function getMany(array $keys, $type)
+		{
+			foreach ($keys as $key)
+				$response[$key] = $this->get($key, $type);
+			return $response;
+		}
+
+		/**
+		 * Method to recover the cache from file or redis
+		 *
 		 * @param $key
 		 * @param string $type
 		 * @return $this|string|BinaryFileResponse
@@ -89,14 +122,15 @@
 		}
 
 		/**
+		 * Private method that decodes the data recovered from the cache, using json decode and if the cachet data is an response will be decoded correctly
+		 *
 		 * @param string $serializedResponse
 		 * @return $this|string|BinaryFileResponse
 		 */
 		private function unserialize(string $serializedResponse)
 		{
 			$responseProperties = json_decode($serializedResponse, true);
-
-			$type = $responseProperties['type'] ?? self::RESPONSE_TYPE;
+			$type = $responseProperties['original']['type'] ?? false;
 			if ($type !== self::RESPONSE_TYPE)
 				return $responseProperties;
 
