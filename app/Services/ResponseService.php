6ab446ef6b0c16690e208a84f73906b29c30aaf9
FabrizioCafolla <fabrizio.cafolla97@gmail.com>
2018-10-19 12:56:12 +0200

    Remove Dingo in response service, fixed array response

--- app/Services/ResponseService.php
+++ app/Services/ResponseService.php
@@ -8,86 +8,126 @@
 
 	namespace App\Services;
 
-	use Dingo\Api\Http\Response\Factory;
+	use Symfony\Component\HttpKernel\Exception\HttpException;
 
 	class ResponseService
 	{
 		/**
-		 * @param string $content
+		 * Method for successful responses, the content parameter is processed by a function that returns the json response message.
+		 * The message will be:
+		 * ["success":["data" or "message": $content, "status_code": $ status]]
+		 *
+		 * @param mixed $content
+		 * @param int $status
+		 * @param array $headers
+		 * @param int $options
 		 * @return \Illuminate\Http\JsonResponse
 		 */
 		public function success($content = "", $status = 200, array $headers = [], $options = 0)
 		{
-			$message = $this->responseData('success', $content, $status);
-			return $this->custom($message, $status, $headers, $options);
+			$message = $this->contentProcessor('success', $content, $status);
+			return $this->response($message, $status, $headers, $options);
 		}
 
 		/**
+		 *
 		 * @param $content
 		 * @param int $status
 		 * @param array $headers
 		 * @param int $options
 		 * @return \Illuminate\Http\JsonResponse
 		 */
-		public function custom($content, $status, array $headers = [], $options = 0)
+		public function response($content, $status, array $headers = [], $options = 0)
 		{
 			return response()->json($content, $status, $headers, $options);
 		}
 
 		/**
+		 * Method for error responses, the content parameter is processed by a function that returns the json response message.
+		 * The message will be:
+		 * ["error":["data" or "message": $content, "status_code": $ status]]
+		 * The parameter $ type is a string used to discriminate the type of error to be returned in the response, it can assume the following values:
+		 * error, errorValidator, errorNotFound, errorBadRequest, errorBadRequest, errorForbidden, errorInternal, errorUnauthorized, errorMethodNotAllowed
+		 *
 		 * @param string $type
 		 * @param string $content
 		 * @param int $status
-		 * @return \Illuminate\Http\JsonResponse|void
+		 * @param array $headers
+		 * @param int $options
+		 * @return \Illuminate\Http\JsonResponse
 		 */
-		public function error($type, $content = "", $status = 400, array $headers = [], $options = 0)
+		public function error($type = "error", $content = "", $status = 400, array $headers = [], $options = 0)
 		{
 			switch ($type) {
-				case "validator":
-					$message = $this->responseData('validator', $content, $status);
-					return $this->custom($message, $status, $headers, $options);
+				case "errorValidator":
+					$message = $this->contentProcessor('error', $content, $status, 'validator');
 					break;
-				case "custom":
-					$message = $this->responseData('error', $content, $status);
-					return $this->custom($message, $status, $headers, $options);
+				case "errorNotFound":
+					$message = $this->contentProcessor('error', $content ? $content:'Not Found', 404);
+					break;
+				case "errorBadRequest":
+					$message = $this->contentProcessor('error', $content ? $content:'Bad Request', 400);
+					break;
+				case "errorForbidden":
+					$message = $this->contentProcessor('error', $content ? $content:'Forbidden', 403);
+					break;
+				case "errorInternal":
+					$message = $this->contentProcessor('error', $content ? $content:'Internal Error', 500);
+					break;
+				case "errorUnauthorized":
+					$message = $this->contentProcessor('error', $content ? $content:'Unauthorized', 401);
+					break;
+				case "errorMethodNotAllowed":
+					$message = $this->contentProcessor('error', $content ? $content:'Method Not Allowed', 405);
 					break;
 				case "error":
-					return $this->factory()->{$type}($content, $status);
-					break;
-				default:
-					return $this->factory()->{$type}($content);
+					$message = $this->contentProcessor('error', $content ? $content:'Generic Error', $status);
 					break;
 			}
+			if(!$message)
+				$this->errorException('Internal Error response, message not found', $status);
+
+			return $this->response($message, $status, $headers, $options);
 		}
 
 		/**
+		 * Method for Exception Errors
+		 *
+		 * @param $message
+		 * @param $status
+		 */
+		public function errorException($message, $status){
+			throw new HttpException($status, $message);
+		}
+
+		/**
+		 * Method for processing the message, used to standardize the basic template facilitating access to the response data.
+		 * If data is array or object add key 'data' else 'message'
+		 * $subArray is a parameter used for multidimensional management of the 'data' key, it is simply a string (which also recognizes the "dot" notation)
+		 *
 		 * @param string $type
 		 * @param $data
-		 * @param $status
+		 * @param null $status
+		 * @param string|null $subArray
 		 * @return array
 		 */
-		private function responseData(string $type, $data, $status)
+		private function contentProcessor(string $type, $data, $status = null, string $subArray = null)
 		{
 			$message = [
 				$type => [
-					'status_code' => $status
 				],
 			];
 
-			if (is_array($data) || is_object($data))
+			if (is_array($data) || is_object($data) && !$subArray)
 				$message[$type] = array_add($message[$type], 'data', $data);
+			elseif (is_array($data) || is_object($data) && $subArray)
+				$message[$type] = array_add($message[$type], 'data.'.$subArray, $data);
 			else
 				$message[$type] = array_add($message[$type], 'message', $data);
 
+			if($status)
+				$message[$type] = array_add($message[$type], 'status_code', $status);
+
 			return $message;
-		}
-		/**
-		 * Get the response factory instance.
-		 *
-		 * @return \Dingo\Api\Http\Response\Factory
-		 */
-		private function factory()
-		{
-			return app(Factory::class);
 		}
 	}
