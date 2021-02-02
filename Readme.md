b9f2d5ace50945cb79240213444c58c79714ae89
FabrzioCafolla <info@fabriziocafolla.com>
2021-02-02 22:16:42 +0100

    fix setup env

--- Readme.md
+++ Readme.md
@@ -6,22 +6,26 @@
 
 ### Description
 
-Microservice Lumen is a starting skeleton based on Docker and Lumen Framework 7. This project helps to develop and maintain a simple and clean infrastructure for the management / creation of php microservices. In just a few steps, the developer starts the development / staging / production environment as needed.
-Basically, the Nginx containers are available for the webserver, the backend container in PHP 7.4 for the application, both based on Linux linux.
+Microservice Lumen is a starting skeleton based on Docker and Lumen Framework. This project helps to develop and maintain a simple and clean infrastructure for the management / creation of php microservices. In just a few steps, the developer starts the development / staging / production environment as needed.
+Basically, the Nginx containers are available for the webserver, the backend container in PHP 7.4 for the application, both based on Linux alpine. Include MySQL container by default.
 The Dockefile (in the docker folder) is already set up to create the production image of the application, we recommend modifying it only to add dependencies or configurations.
 
 #### Develop env
+
+La prima volta che viene eseguito il setup saranno chiesti in input dei dati (nome app, verisone larvel ed altro), questo genererà un file env.conf e un .env (che è la copia esetta), il primo dovrete versionarlo il secondo sarà escluso. Inoltre verrà scaricato il codice sorgente [laravel/lumen](https://github.com/laravel/lumen/), anche'esso dovrà essere versionato. Nel caso il vostro progetto sia stato già inizializzato, chi andrà a scaricarlo dovrà eseguire comunque sia il primo passo indicato in 'setup and run'.
+
 **- require**
     
+    OS: linux
+    Packages: make
     Docker version: >= 18.09.6
     docker-compose version: >= 1.28.0
 
 **- setup and run**
 
-    1.  Edit .env.develop config and copy in root directory
-        make setup
+    1.  make setup
 
-    2.  Set .env db connection into lumen/ dir:
+    2.  [not required] Set .env db connection into lumen/ dir:
         DB_CONNECTION=mysql
         DB_HOST=mysql.private
         DB_PORT=3306
@@ -36,7 +40,9 @@ The Dockefile (in the docker folder) is already set up to create the production 
 
     up:   up -d containers
 
-    exec:  enter into lumen container
+    exec:  enter in app container
+
+    exec_mysql:  enter in mysql container
 
     ssh_root:  connection ssh (as root) to server
 
@@ -57,9 +63,11 @@ The Dockefile (in the docker folder) is already set up to create the production 
     
 [base image](https://hub.docker.com/r/fabriziocaf/lumen)
 
-**Webserver Nginx**: 1.18
+**Nginx**: 1.18
 
-**Application: PHP**: 7.4-fpm-alpine
+**PHP**: 7.4-fpm-alpine
+
+**MySQL**: 8.0
 
 **Lumen Framework**: delfault master (or specific version)
 
--- setup.sh
+++ setup.sh
@@ -12,6 +12,7 @@ trap 'failure ${LINENO} "$BASH_COMMAND"' ERR
 set -o pipefail
 
 ENV_VALIDATOR=("dev" "sta" "pro")
+FILE_ENV_CONF="./env.cfg"
 FILE_ENV="./.env"
 FILE_ENV_EXAMPLE="./example.env"
 SOURCEPATH="./source"
@@ -79,28 +80,28 @@ update_env() {
 set_dev_env() {
   read -p 'SSH key (enter to skip): ' SSHKEY
   if [ "${SSHKEY}" == "" ] ; then 
-    echo "#SETTING DEV SKIPPED" >> $FILE_ENV
+    echo "#SETTING DEV SKIPPED" >> $FILE_ENV_CONF 
     exit 0
   fi 
 
   if [ -f ${SSHKEY} ] ; then
-    update_env $FILE_ENV "SSHKEY" $SSHKEY
+    update_env $FILE_ENV_CONF  "SSHKEY" $SSHKEY
   else
     echo "${SSHKEY} non trovata"
     exit 1
   fi
   
   read -p 'USER SERVER (root or sudo user): ' SRV_USER
-  update_env $FILE_ENV "SRV_USER" $SRV_USER
+  update_env $FILE_ENV_CONF  "SRV_USER" $SRV_USER
     
   read -p 'USER SERVER WORKDIR: ' SRV_USER_WK
   if [ "${SRV_USER_WK}" == "" ] ; then
     $SRV_USER_WK=$SRV_USER
   fi
-  update_env $FILE_ENV "SRV_USER_WK" $SRV_USER_WK
+  update_env $FILE_ENV_CONF  "SRV_USER_WK" $SRV_USER_WK
 
   read -p 'AWS IP EC2: ' SRV_HOST
-  update_env $FILE_ENV "SRV_HOST" $SRV_HOST
+  update_env $FILE_ENV_CONF  "SRV_HOST" $SRV_HOST
 
 
 	cp ${SOURCEPATH}/.env.example ${SOURCEPATH}/.env
@@ -122,31 +123,37 @@ download_wp(){
 }
 
 main() {
-  if [ -f "$FILE_ENV" ] ; then
-    echo "[ERROR] file .env exist"
-    exit 1
+  echo "[INFO] setup env"
+
+  if [ -f "$FILE_ENV_CONF" ] ; then
+    cp $FILE_ENV_CONF $FILE_ENV
+    exit 0
+  else
+    echo "[INFO] First init"
   fi
 
   parser $@
 
-  touch $FILE_ENV
+  touch $FILE_ENV_CONF 
 
-  update_env $FILE_ENV "ENV" "$ENV"
-  update_env $FILE_ENV "APPNAME" "$APPNAME"
-  update_env $FILE_ENV "DOMAIN" "$DOMAIN"
-  update_env $FILE_ENV "DOCKERFILE_PATH" "."
-  update_env $FILE_ENV "WORKDIR_USER" "www-data"
-  update_env $FILE_ENV "WORKDIR_GROUP" "www-data"
-  update_env $FILE_ENV "WORKDIRPATH" "/var/www"
-  update_env $FILE_ENV "SOURCEPATH" ${SOURCEPATH}
-  update_env $FILE_ENV "CONTAINERPATH" "./container"
-  update_env $FILE_ENV "VOLUMESPATH" "./container/data"
-  update_env $FILE_ENV "IMAGENAME" "microservice/${APPNAME}"
+  update_env $FILE_ENV_CONF  "ENV" "$ENV"
+  update_env $FILE_ENV_CONF  "APPNAME" "$APPNAME"
+  update_env $FILE_ENV_CONF  "DOMAIN" "$DOMAIN"
+  update_env $FILE_ENV_CONF  "DOCKERFILE_PATH" "."
+  update_env $FILE_ENV_CONF  "WORKDIR_USER" "www-data"
+  update_env $FILE_ENV_CONF  "WORKDIR_GROUP" "www-data"
+  update_env $FILE_ENV_CONF  "WORKDIRPATH" "/var/www"
+  update_env $FILE_ENV_CONF  "SOURCEPATH" ${SOURCEPATH}
+  update_env $FILE_ENV_CONF  "CONTAINERPATH" "./container"
+  update_env $FILE_ENV_CONF  "VOLUMESPATH" "./container/data"
+  update_env $FILE_ENV_CONF  "IMAGENAME" "microservice/${APPNAME}"
 
   if [ "$ENV" == "dev" ] ; then
     download_wp
     set_dev_env
   fi
+
+  cp $FILE_ENV_CONF $FILE_ENV
 
   exit 0
 }
